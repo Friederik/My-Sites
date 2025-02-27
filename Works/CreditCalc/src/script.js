f2b9514961fc9b0ed06a8f60f0d6757d3f755c29
Friederik <mr.ilya2003@yandex.ru>
2025-02-27 21:34:16 +0500

    Credit Calc #2
    Английский надоел (описания на русском пишу), Сделана печать, Расчет по месяцам, Добавлен не работающий выбор даты

--- Works/CreditCalc/src/script.js
+++ Works/CreditCalc/src/script.js
@@ -1,12 +1,24 @@
+import * as flatpickr from 'https://cdn.jsdelivr.net/npm/flatpickr';
+
+function setup() {
+    
+}
+
 document.getElementById("submit-button").addEventListener("click", function () {
 
     let amountObj = document.getElementById("amount");
     let rateObj = document.getElementById("rate");
     let monthsObj = document.getElementById("months");
     let rangeObj = document.getElementById("range");
+    let dateCreditObj = document.getElementById("date-credit");
 
-    calculateLoan(amountObj, rateObj, monthsObj, rangeObj);
+
+    calculateLoan(amountObj, rateObj, monthsObj, rangeObj, dateCreditObj);
 });
+
+document.getElementById("print-button").addEventListener('click', function() {
+    window.print();
+})
 
 document.getElementById("currency").addEventListener('change', function(){
     document.querySelectorAll(".currency-val").forEach(val => {
@@ -15,18 +27,29 @@ document.getElementById("currency").addEventListener('change', function(){
     })
 });
 
-function calculateLoan(amountObj, rateObj, monthsObj, rangeObj) {
+document.getElementById("range").addEventListener('change', function(){
+    let val = this.options[this.selectedIndex].value
+    if(val === "years") {
+        document.getElementById("months").placeholder = "Введите к. лет"
+    }
+    else {
+        document.getElementById("months").placeholder = "Введите к. месяцев"
+    }
+    
+});
+
+function calculateLoan(amountObj, rateObj, monthsObj, rangeObj, dateCreditObj) {
     let amount = amountObj.value;
     let rate = rateObj.value;
     let months = monthsObj.value;
+    let dateCredit = dateCreditObj.value;
+
+    let numbers = [amountObj, rateObj, monthsObj, dateCreditObj]
+
     let range = rangeObj.value;
     if(range === 'years') {
         months *= 12
     }
-
-    console.log(months)
-
-    let numbers = [amountObj, rateObj, monthsObj]
 
     let flag = true
     numbers.forEach(num => {
@@ -36,15 +59,55 @@ function calculateLoan(amountObj, rateObj, monthsObj, rangeObj) {
     })
 
     if(flag === false) return
-    let monthlyRate = rate / 100 / 12;
-    let payment = (amount * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -months));
-    let allPayments = payment * months
-    let overpayment = allPayments - amount
 
-    document.getElementById("sum-in-month-num").innerText = payment.toFixed(2)
-    document.getElementById("sum-all-num").innerText = allPayments.toFixed(2)
-    document.getElementById("overpayment-num").innerText = overpayment.toFixed(2)
+    let monthlyRate = rate / 100 / 12
+    let payment = ((amount * monthlyRate) / (1 - Math.pow(1 + monthlyRate, -months))).toFixed(0)
+    let allPayments = (payment * months).toFixed(0)
+    let overpayment = (allPayments - amount).toFixed(0)
+
+    document.getElementById("sum-in-month-num").innerText = payment
+    document.getElementById("sum-all-num").innerText = allPayments
+    document.getElementById("overpayment-num").innerText = overpayment
     document.getElementById("overpayment-percent-num").innerText = (overpayment / amount * 100).toFixed(3)
 
     document.getElementById("conclusion").style.display = "block"
+    document.getElementById("print-button").style.color = "white"
+
+    console.log(amount)
+    calculateLoanTable(months, monthlyRate, payment, amount)
 }
+
+function calculateLoanTable(months, monthlyRate, payment, amount) {
+    document.getElementById('conclusion-table').innerHTML = `
+        <tr>
+            <th>Дата</th>
+            <th>Платеж</th>
+            <th>Процент</th>
+            <th>Основной долг</th>
+            <th>Остаток</th>
+        </tr>        
+    `
+    for(let i = 0; i < months; i++) {
+        let row = document.createElement('tr')
+
+        let percents = (amount * monthlyRate).toFixed(0)
+        let mainCredit = (payment - percents).toFixed(0)
+        if(i === months-1) {
+            mainCredit = amount
+            payment = mainCredit * (1 + monthlyRate).toFixed(0)
+        }
+        amount -= mainCredit
+        console.log(i, months-1,monthlyRate, amount,percents, mainCredit)
+
+        row.innerHTML = `
+            <td>27.${i}.2025</td>
+            <td>${payment}</td>
+            <td>${percents}</td>
+            <td>${mainCredit}</td>
+            <td>${amount}</td>
+        `
+        document.getElementById('conclusion-table').appendChild(row)
+    }
+}
+
+setup()
